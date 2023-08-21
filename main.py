768a6094f94f87423872e36dadf816226037f3f0
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-21 20:57:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,4 @@ CHANNEL_ID = 114278
 jokes_api_url = 'https://v2.jokeapi.dev/joke/Any'  # Change 'Any' to a specific category if desired
 
 intents = discord.Intents.default()
+intents.typing = False
